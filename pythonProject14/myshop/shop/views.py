f0f78a9dd4cc840a2397fb203fe8c3a4d8b03c1f
GalakhovOA <146749149+GalakhovOA@users.noreply.github.com>
2024-08-22 23:26:37 +0300

    Add files via upload

---
+++ pythonProject14/myshop/shop/views.py
@@ -0,0 +1,68 @@
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'shop/product_list.html', {'products': products})
+
+def add_to_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    cart_item, created = CartItem.objects.get_or_create(product=product)
+    cart_item.quantity += 1
+    cart_item.save()
+    return redirect('product_list')
+
+def cart_view(request):
+    cart_items = CartItem.objects.all()
+    total_price = sum(item.product.price * item.quantity for item in cart_items)
+    return render(request, 'shop/cart.html', {'cart_items': cart_items, 'total_price': total_price})
+
+
+from django.contrib.auth import login
+from .forms import UserRegistrationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.set_password(form.cleaned_data['password1'])
+            new_user.save()
+            login(request, new_user)
+            return redirect('product_list')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'shop/register.html', {'form': form})
+
+from django.shortcuts import render
+from .models import Product
+
+def home_page(request):
+    products = Product.objects.all()
+    cart_items = CartItem.objects.all()
+    return render(request, 'shop/home_page.html', {'products': products, 'cart_items': cart_items})
+
+
+
+from django.shortcuts import get_object_or_404, redirect
+from .models import CartItem
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def remove_from_cart(request, item_id):
+    cart_item = get_object_or_404(CartItem, id=item_id)
+    cart_item.delete()
+    return redirect('cart_view')
+
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('product_list')  # Замените на нужный URL
+    else:
+        form = AuthenticationForm()
+    return render(request, 'shop/login.html', {'form': form})
